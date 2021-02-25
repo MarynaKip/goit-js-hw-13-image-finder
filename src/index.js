3a19dd3768045e68ebaa29ef24f2096d4f9f8fe3
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2021-02-25 21:25:47 +0200

    scroll changes added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import picturesTemplate from './templates/pictureTemplate.hbs';
 const refs = {
   searchForm: document.querySelector('.search-form'),
   galleryContainer: document.querySelector('.gallery'),
+  scrollButton: document.querySelector('.scrollButton'),
 };
 
 const newPixabayApi = new NewPixabayApi();
@@ -13,6 +14,8 @@ const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
   hidden: true,
 });
+
+const heightArray = [];
 
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', () => {
@@ -36,8 +39,26 @@ function fetchPictures() {
 
   newPixabayApi.fetchPictures().then(elements => {
     appendPictures(elements);
+
+    // console.log(newPixabayApi.page);
+
+    heightArray.push(document.documentElement.offsetHeight);
+    console.log(heightArray);
+    const element = newPixabayApi.page - 2;
+    const height =
+      // refs.galleryContainer.scrollHeight -
+      document.documentElement.offsetHeight -
+      (heightArray[element] - heightArray[element - 1]) -
+      refs.scrollButton.clientHeight +
+      refs.galleryContainer.lastElementChild.clientHeight;
+    // console.log(document.documentElement.offsetHeight);
+    // console.log(heightArray[element] - heightArray[element - 1]);
+    // console.log(refs.scrollButton.clientHeight);
+    // console.log(refs.galleryContainer.lastElementChild.clientHeight);
+    // console.log(height);
+
     window.scrollTo({
-      top: document.documentElement.offsetHeight,
+      top: height,
 
       behavior: 'smooth',
     });
